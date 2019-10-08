c23eeb681efe4f33098260b50351fb98b9574cb0
matthewr6745 <56267102+matthewr6745@users.noreply.github.com>
2019-10-08 12:00:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Matthew Regan"
+let userName = "matthewr6745";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
